6f1b0f3bd39668f1656d59750da4898ec38d727e
Roccor4 <jackphil4l@gmail.com>
2025-08-02 15:10:35 +0200

    Update HeroSection.js

--- frontend/src/components/HeroSection.js
+++ frontend/src/components/HeroSection.js
@@ -52,12 +52,13 @@ const HeroSection = () => {
       </div>
 
       {/* Scroll indicator */}
-<div className="absolute bottom-8 left-1/2 transform -translate-x-[52%] animate-bounce">
+<div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
   <svg
     className="w-6 h-6"
     fill="none"
     stroke="currentColor"
     viewBox="0 0 24 24"
+    style={{ transform: 'translateX(-1px)' }} // nudge left 1px
   >
     <path
       strokeLinecap="round"
