b9a9b304f48538eccbdd8191d6c5bd848ec2f523
Roccor4 <jackphil4l@gmail.com>
2025-08-02 15:07:41 +0200

    Update HeroSection.js

--- frontend/src/components/HeroSection.js
+++ frontend/src/components/HeroSection.js
@@ -52,7 +52,7 @@ const HeroSection = () => {
       </div>
 
       {/* Scroll indicator */}
-<div className="absolute bottom-8 left-1/2 transform -translate-x-1/2 animate-bounce">
+<div className="absolute bottom-8 left-1/2 transform -translate-x-[52%] animate-bounce">
   <svg
     className="w-6 h-6"
     fill="none"
